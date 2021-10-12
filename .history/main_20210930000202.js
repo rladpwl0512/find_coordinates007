920d01b7fe70af694b0d780aeac202c6fb3fe023
KIM YEJI <52344833+rladpwl0512@users.noreply.github.com>
2021-10-13 02:07:31 +0900

    Delete .history directory

--- .history/main_20210930000202.js
+++
@@ -1,5 +0,0 @@
-const vertical = document.querySelector('.vertical');
-const horizon = document.querySelector('.horizon');
-const target = document.querySelector('.target');
-const tag = document.querySelector('.tag');
-
